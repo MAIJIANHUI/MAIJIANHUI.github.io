bcaec69562c1e94d12a6d9606beb80f489407c3e
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2022-03-03 23:04:55 +0800

    Delete time.js

--- time.js
+++
@@ -1,5 +0,0 @@
-<!-- Begin
-copyright=new Date();//取得当前的日期
-update=copyright.getFullYear();//取得当前的年份
-document.write("Copyright@2004-"+ update + "MAIJIANHUI All rights reserved.");//update为自动更新的年份
-//  End -->
