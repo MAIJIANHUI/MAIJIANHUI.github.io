50437e3f2a6d3ff38b8a45f7244c948db8accb3f
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2022-04-02 20:25:01 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,5 +1,5 @@
 <!-- Begin
 copyright=new Date();//取得当前的日期
 update=copyright.getFullYear();//取得当前的年份
-document.write("COPYRIGHT "+ update + " MAIJIANHUI");//update为自动更新的年份
+document.write("COPYRIGHT "+ update + " <a href="https://www.maijianhui.com/blog/yulu.html" arget="_blank">MAIJIANHUI</a>");//update为自动更新的年份
 //  End -->
