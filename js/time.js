4bf325f4965e8ad1331e27a5517c8a39d94743d9
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2022-09-08 14:52:37 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,6 +1,3 @@
-function addScript(url){
-    document.write("<script language=javascript src="https://www.maijianhui.com/js/automatic.js"></script>");
-};
 copyright=new Date();
 update=copyright.getFullYear();
 document.write("COPYRIGHT "+ update + " MAIJIANHUI");
