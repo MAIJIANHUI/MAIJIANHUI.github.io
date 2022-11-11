d04aabb171c694ef40a5f28683980e2f97690d28
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2022-11-11 11:12:33 +0800

    Update htmer_time.js

--- js/htmer_time.js
+++ js/htmer_time.js
@@ -28,7 +28,7 @@ function secondToDate(second) {
         var create_time = Math.round(new Date(Date.UTC(2020, 02, 06, 00, 00, 00)).getTime() / 1000);
         var timestamp = Math.round((new Date().getTime() + 8 * 60 * 60 * 1000) / 1000);
         currentTime = secondToDate((timestamp - create_time));
-        currentTimeHtml ='已运行：' currentTime[0] + '年' + currentTime[1] + '天'
-                + currentTime[2] + '时' + currentTime[3] + '分';
+        currentTimeHtml ='&#12300;' + '已运行：' currentTime[0] + '年' + currentTime[1] + '天'
+                + currentTime[2] + '时' + currentTime[3] + '分' + '&#12301;';
         document.getElementById("htmer_time").innerHTML = currentTimeHtml;
     }    setInterval(setTime, 1000);
