8d897fcbb4a1f327736c310e193d20954ce9eb60
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2022-12-01 20:47:43 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -41,10 +41,5 @@ function IEVersion() {
     },0)
 };
 
- jinrishici.load(function(result) {
-    var sentence = document.querySelector("#poem_sentence")
-    var info = document.querySelector("#poem_info")
-    info.innerHTML = '&#12300;&nbsp;' + result.data.matchTags[0]||相思+ '&nbsp;&#12301;'
-  });
-
+ 
 
