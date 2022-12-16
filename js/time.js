b8066620a587b69eacc7383ee1fe82c6d5ba0b76
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2022-12-16 09:29:22 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -56,7 +56,7 @@ if(aidaori()){
 }
 
 function aidaori(){
-    var aidaoriarr=new Array("0108","0219","0403","0404","0405","0406","0414","0512","0522","0807","0909","1130","1206","1213");
+    var aidaoriarr=new Array("0108","0219","0403","0404","0405","0406","0414","0512","0522","0807","0909","1130","1213");
     var mydate = new Date();
     var str = "";// + mydate.getFullYear();
     var mm = mydate.getMonth()+1;
