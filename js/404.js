46740b018e04214d3c3bf444c07ce3f27f0cc3f4
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2022-12-19 15:43:39 +0800

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#text404', {
-    strings: ["&#12300;知不可乎骤得，托遗响于悲风&#12300;"],
+    strings: ["&#12300;知不可乎骤得，托遗响于悲风&#12301;"],
     cursorChar: '',
     typeSpeed: 150
     });
