0f42c6311e62485a68ac524dc2993ed0d9279d82
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2022-12-19 15:21:59 +0800

    Update hitokoto.js

--- js/hitokoto.js
+++ js/hitokoto.js
@@ -16,9 +16,4 @@ xhr.onreadystatechange = function () {
 xhr.send();
 
 
- var typed = new Typed('#text-404', {
-    strings: ["&#12300;知不可乎骤得，托遗响于悲风&#12300;"],
-    cursorChar: '',
-    typeSpeed: 150
-    });
-
+ 
