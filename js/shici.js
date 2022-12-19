93342fb0adef9b039dad77be651ae06348f8553a
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2022-12-19 10:00:45 +0800

    Update shici.js

--- js/shici.js
+++ js/shici.js
@@ -1,16 +1,16 @@
 var xhr = new XMLHttpRequest();
-  xhr.open('get', 'https://v2.jinrishici.com/one.json');
-  xhr.withCredentials = true;
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      var data = JSON.parse(xhr.responseText);
-      var gushici = document.getElementById('shici');
-       var shici = data.data.content;
-    }
-    var typed = new Typed('#shici', {
-    strings: [shici],
+xhr.open('get', 'https://v1.hitokoto.cn');
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    const data = JSON.parse(xhr.responseText);
+    const hitokoto = document.querySelector('#hitokoto_text');
+    hitokoto.href = `https://hitokoto.cn/?uuid=${data.uuid}`
+    var hitokoto_text = data.hitokoto;
+  }
+    var typed = new Typed('#hitokoto_text', {
+    strings: [hitokoto_text],
     cursorChar: '',
-    typeSpeed: 300
+    typeSpeed: 200
     });
-  };
-  xhr.send();
+};
+xhr.send();
