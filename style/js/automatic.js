40d97bc64e5136393a98e7f7dc6c5b58e334ba09
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2022-12-20 17:19:18 +0800

    Add files via upload

---
+++ style/js/automatic.js
@@ -0,0 +1,17 @@
+ var time = 30
+    var divLabel = document.getElementById("countdown")
+    var label = "秒后将返回首页"
+    divLabel.innerHTML = time.toString() + label
+    function updateTime(){
+        time = time-1
+        if(time>=0){ divLabel.innerHTML = time.toString() + label }
+        else{ divLabel.innerHTML = "正在跳转，任何问题请联系我！<br/><br/>a@mai.ac.cn<br/><br/>请稍后！" }
+    }
+    setInterval("updateTime()",1000);
+    setTimeout("javascript:location.href='https://www.maijianhui.com'", time*1000);
+
+var typed = new Typed('#text404', {
+    strings: ["知不可乎骤得，托遗响于悲风"],
+    cursorChar: '',
+    typeSpeed: 200
+    });
---
+++ style/js/hitokoto.js
@@ -0,0 +1,19 @@
+var xhr = new XMLHttpRequest();
+xhr.open('get', 'https://v1.hitokoto.cn');
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    const data = JSON.parse(xhr.responseText);
+    const hitokoto = document.querySelector('#hitokoto_text');
+    hitokoto.href = `https://hitokoto.cn/?uuid=${data.uuid}`
+    var hitokoto_text = data.hitokoto;
+  }
+    var typed = new Typed('#hitokoto_text', {
+    strings: [hitokoto_text],
+    cursorChar: '',
+    typeSpeed: 150
+    });
+};
+xhr.send();
+
+
+ 
