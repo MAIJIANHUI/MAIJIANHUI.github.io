7ed889089d32355528b4ce70d81fbe281f680a6d
JIANHUI <34123919+MAIJIANHUI@users.noreply.github.com>
2023-04-21 16:19:52 +0800

    Update automatic.js

--- styles/js/automatic.js
+++ styles/js/automatic.js
@@ -5,7 +5,7 @@
     function updateTime(){
         time = time-1
         if(time>=0){ divLabel.innerHTML = time.toString() + label }
-        else{ divLabel.innerHTML = "正在跳转，任何问题请联系我！<br/><br/>a@mai.ac.cn<br/><br/>请稍后！" }
+        else{ divLabel.innerHTML = "正在跳转，任何问题请联系我！<br/><br/>web@mai.ac.cn<br/><br/>请稍后！" }
     }
     setInterval("updateTime()",1000);
     setTimeout("javascript:location.href='https://mai.ac.cn'", time*1000);
